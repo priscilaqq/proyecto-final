8d02aad22ea7468526431b05b3b0e5bc7bbf784c
gabrielalberini <gabialberini733@gmail.com>
2025-07-22 17:20:43 -0300

    add: contenido estático en login, creación del form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import { Layout } from "../components/Layout"
 const Login = () => {
   return (
     <Layout>
-      <h1>Hola desde login</h1>
+      <h1>Inicia sesión</h1>
+
+      <section>
+        <h2>Hola, bienvenido de nuevo</h2>
+        <form>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }

cc77fe80d8f9d2e17aa2fb165f8525df47b7d7c4
gabrielalberini <gabialberini733@gmail.com>
2025-07-22 17:23:08 -0300

    add: contenido estático en register, preparación del form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,26 @@ import { Layout } from "../components/Layout"
 const Register = () => {
   return (
     <Layout>
-      <h1>Hola desde register</h1>
+      <h1>Registrate</h1>
+
+      <section>
+        <h2>Hola, bienvenido</h2>
+        <form>
+          <div>
+            <label>Username:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }

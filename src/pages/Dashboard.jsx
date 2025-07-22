ffd4c5c057aec0003544fcebf7cec9768e488fd9
gabrielalberini <gabialberini733@gmail.com>
2025-07-22 17:16:45 -0300

    add: contenido estático en dashboard, preparación de form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,29 @@ import { Layout } from "../components/Layout"
 const Dashboard = () => {
   return (
     <Layout>
-      <h1>Hola desde dashboard</h1>
+      <h1>Panel de Administración</h1>
+
+      <section>
+        <h2>Cargar nuevo producto</h2>
+        <form>
+          <div>
+            <label>Nombre del producto:</label>
+            <input type="text" name="nombre" />
+          </div>
+
+          <div>
+            <label>Precio:</label>
+            <input type="number" name="precio" />
+          </div>
+
+          <div>
+            <label>Descripción:</label>
+            <textarea name="descripcion" rows="4" />
+          </div>
+
+          <button>Guardar producto</button>
+        </form>
+      </section>
     </Layout>
   )
 }

a22c458ed34ba5c0c08740e378651e44f10afac3
priscilaqq <abpriscilaq@gmail.com>
2025-08-14 01:33:38 -0300

    modificacion de css de header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,20 @@
-import "../styles/pages/Footer.css"
+import "../styles/components/Footer.css"
 
 const Footer = () => {
   return (
-    <section>
     <footer className="footer">
-    <div className="footer-content">
-      <h2 className="logo">Mi Proyecto</h2>
-      <nav className="footer-links">
-        <a href="#about">Sobre nosotros</a>
-        <a href="#services">Servicios</a>
-        <a href="#contact">Contacto</a>
-      </nav>
-      <p className="footer-copy">
-        © {new Date().getFullYear()} Mi Proyecto — Todos los derechos reservados.
-      </p>
-    </div>
-      </footer>
-      </section>
+      <div className="footer-content">
+        <h2 className="logo">Mi Proyecto</h2>
+        <nav className="footer-links">
+          <a href="#about">Sobre nosotros</a>
+          <a href="#services">Servicios</a>
+          <a href="#contact">Contacto</a>
+        </nav>
+        <p className="footer-copy">
+          © {new Date().getFullYear()} Mi Proyecto — Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
   )
 }
 
